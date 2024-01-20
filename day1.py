4b1f51011b5918276319279029619c1f44a3e8e1
Ayush Sachin Patil <110882066+ayushsp29@users.noreply.github.com>
2024-01-20 10:40:15 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,45 @@
+# #Day 1- of 100 days of code in python
+
+# print('Hello Wolrd')
+# #Print syntax- print('') or print(""), "" marks the beigining and the end of the string, in this case the string is of char.
+# print("Hello"+' Ayush')
+
+# """
+# Question
+# # Fix the code below 👇
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+# Solution
+# # Fix the code below 👇
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+# """
+
+# #INPUT FUNTION
+# #input(), example:
+
+# print("Hello "+ input("What is your name?"))
+
+#printing the number of letters in the string
+# s='Ayush'
+# len(s)
+# print(s)
+# print(len(s))
+# print(len('Hello'))
+
+#1. Create a greeting for your program.
+print('Hello, Welcome to the Band Name Generator')
+#2. Ask the user for the city that they grew up in.
+city = input("Whats your city name?\n")
+#3. Ask the user for the name of a pet.
+pet = input("Whats your pet name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("your band could be " + city + " "+ pet)
+#5. Make sure the input cursor shows on a new line:
