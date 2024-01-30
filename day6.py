8644cfa497287ba958fb496bddc01dad01c80fb6
Ayush Sachin Patil <110882066+ayushsp29@users.noreply.github.com>
2024-01-30 21:44:15 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,11 @@
+"""
+DAY 6 - Functions
+"""
+#Functions, While loops, Code Blocks
+#syntax - def <name_of_the_function>():
+#Example-
+def my_function():
+    print('Hello')
+    print('World!')
+
+my_function()
